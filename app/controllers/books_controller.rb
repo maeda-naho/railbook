fa7514b698b54ba5ea25050b6601d792c8264164
Naho Maeda <maeda-naho@outlook.jp>
2016-06-13 12:21:26 +0900

    rensyu1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -66,6 +66,10 @@
 render :index
   end
 
+ def view_helper_test
+  
+end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ﻿Rails.application.routes.draw do
   resources :books do
  get 'search', on: :collection
+ get 'view_helper_test', on: :collection
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
